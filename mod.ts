edab9bc053f5cf90019fd4787a08fedcb69fe1e1
Tom Wilson <twilson63@gmail.com>
2021-03-09 10:03:53 -0500

    update action

--- mod.ts
+++ mod.ts
@@ -1,65 +1,61 @@
-
 interface m {
-  of: Function,
-  map: Function,
-  chain: Function,
-  ap: Function,
-  [x: string]: any
+  of: Function;
+  map: Function;
+  chain: Function;
+  ap: Function;
+  [x: string]: any;
 }
 
-
-type M = { (...args: any): any; of: Function; [x: string]: any }
+type M = { (...args: any): any; of: Function; [x: string]: any };
 // Function with one argument
-type U = { ([x, string]: any) : any } 
+type U = { ([x, string]: any): any };
 
-export default function _ReaderT(Monad : M ) {
-  const typestring = `ReaderT(${Monad['@@type']})`
+export default function _ReaderT(Monad: M) {
+  const typestring = `ReaderT(${Monad["@@type"]})`;
 
-  const of = (x : any) => ReaderT(() => Monad.of(x))
-  
+  const of = (x: any) => ReaderT(() => Monad.of(x));
+
   function ask(fn: U) {
-    return ReaderT(Monad.of).map(fn)
+    return ReaderT(Monad.of).map(fn);
   }
 
   function lift(m: m) {
-    return ReaderT(() => m)
+    return ReaderT(() => m);
   }
 
   function ReaderT(wrapped: Function) {
-    function runWith(x : any) {
-      return wrapped(x)
+    function runWith(x: any) {
+      return wrapped(x);
     }
-    
-    function map(fn : U) {
-      return ReaderT(( e: any) => runWith(e).map(fn))
+
+    function map(fn: U) {
+      return ReaderT((e: any) => runWith(e).map(fn));
     }
-    
-    function ap(m : m) {
-      return ReaderT((e : any) => runWith(e).ap(m.runWith(e)))
+
+    function ap(m: m) {
+      return ReaderT((e: any) => runWith(e).ap(m.runWith(e)));
     }
 
     function chain(fn: U) {
-      return ReaderT((e : any) =>
-        runWith(e).chain((inner : any) => fn(inner).runWith(e))
-      )
+      return ReaderT((e: any) =>
+        runWith(e).chain((inner: any) => fn(inner).runWith(e))
+      );
     }
 
     return {
       runWith,
-      of, 
+      of,
       map,
       ap,
       chain,
-      ['@@type']: typestring,
-      constructor: ReaderT
-    }
-
-          
+      ["@@type"]: typestring,
+      constructor: ReaderT,
+    };
   }
 
-  ReaderT.of = of
-  ReaderT.ask = ask
-  ReaderT.lift = lift
-  ReaderT['@@type'] = typestring
-  return ReaderT
+  ReaderT.of = of;
+  ReaderT.ask = ask;
+  ReaderT.lift = lift;
+  ReaderT["@@type"] = typestring;
+  return ReaderT;
 }
--- mod_test.ts
+++ mod_test.ts
@@ -1,22 +1,22 @@
-import { assertEquals } from './deps.ts'
-import { Task } from './deps.ts'
-import ReaderT from './mod.ts'
+import { assertEquals } from "./deps.ts";
+import { Task } from "./deps.ts";
+import ReaderT from "./mod.ts";
 
-const TaskReader = ReaderT(Task)
-const { of, ask, lift } = TaskReader
+const TaskReader = ReaderT(Task);
+const { of, ask, lift } = TaskReader;
 
-Deno.test('hello', () => {
-  of('hello')
+Deno.test("hello", () => {
+  of("hello")
     .map((v: string) => v.toUpperCase())
-    .chain((v : string) => 
+    .chain((v: string) =>
       ask(
-        (env : any) => env.exclaim ? Task.resolve(`${v}!`) : Task.resolve(v) 
+        (env: any) => env.exclaim ? Task.resolve(`${v}!`) : Task.resolve(v),
       )
     )
-    .chain((task : any) => lift(task))
-    .runWith({ exclaim: true})
+    .chain((task: any) => lift(task))
+    .runWith({ exclaim: true })
     .fork(
       (e: string) => assertEquals(false, true),
-      (r: string) => assertEquals(r, 'HELLO!')
-    )
-})
+      (r: string) => assertEquals(r, "HELLO!"),
+    );
+});
